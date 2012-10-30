17ecbaae372acc2252a8f9e4ceef4a4b570f4d0b
kathrynalpert <kathrynialpert@gmail.com>
2012-10-30 12:53:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redneckmountaineering::Application.config.secret_token = '4cd763e88cded45c1fa2f9f07985b0913ad6e665a8f87b139e9beb4cb712af3ce12d703abe4ea38e4e873a157dbe15d473d919e03fd7a4e5125bdc353d562929'
