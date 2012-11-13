88a67310143256631b10a4364ca8fa958f6e8713
kathrynalpert <kathrynialpert@gmail.com>
2012-11-12 21:13:29 -0700

    add page images engine; rework blog display

--- config/initializers/refinery/blog.rb
+++ config/initializers/refinery/blog.rb
@@ -3,9 +3,9 @@ Refinery::Blog.configure do |config|
 
   # config.comments_per_page = 10
 
-  # config.posts_per_page = 10
+   config.posts_per_page = 3
 
-  # config.post_teaser_length = 250
+   config.post_teaser_length = 300
 
   # config.share_this_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 end
